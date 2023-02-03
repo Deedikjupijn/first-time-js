9fbbad08bd31945ebeadcb7987fd14d02e880839
Deedikjepijn <joris8393@gmail.com>
2023-02-03 11:37:20 +0100

    rekenmachine gemaakt!

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
+    document.getElementById("changepara").innerHTML = "Paragraph changed.";
+}
+
+function rekenUit() {
+    var x = document.getElementById("getal1").value;
+    var y = document.getElementById("getal2").value;
+    var uitkomst = parseInt(x) + parseInt(y);
+    document.getElementById("uitkomst").innerHTML = uitkomst;
 }
